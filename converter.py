558228db73343422222ac429511a6669e5766716
Francois Lesage <francois.lesage@gmail.com>
2020-10-03 23:34:37 +0200

    sort meteorite landings by year

--- converter.py
+++ converter.py
@@ -5,38 +5,26 @@ import datetime
 import pytz
 import dateutil
 import dateutil.parser
+from operator import itemgetter
+from collections import OrderedDict
 
 # read NASA datas
 with open('nasa-datas.json', 'r') as j:
 	nasaevents = json.load(j)
 
+
 for nasaevent in nasaevents:
 	if "year" in nasaevent and "mass" in nasaevent and "geolocation" in nasaevent:
-		#print("nasaevent:" + nasaevent['year'])
-		# 1995-01-01T00:00:00.000
-		#print(nasaevent['year'].split("-", 1)[0])
-		
-		t = "2016-05-24 11:30 PST"
-
-		#datetime_object = datetime.strptime(nasaevent['year'].split("-", 1)[0], '%Y')
-		#seconds_since_epoch = datetime.datetime.now().timestamp()
-		#milliseconds_since_epoch = datetime.datetime.now().timestamp() * 1000
-		milliseconds_since_epoch = str(datetime.datetime(int(nasaevent['year'].split("-", 1)[0]), 1, 1).timestamp() * 1000)
+		milliseconds_since_epoch = int(datetime.datetime(int(nasaevent['year'].split("-", 1)[0]), 1, 1).timestamp() * 1000)
 		nasaevent['time'] = milliseconds_since_epoch
 		nasaevent['lat'] = nasaevent['geolocation']['latitude']	
 		nasaevent['lng'] = nasaevent['geolocation']['longitude']	
 		nasaevent['mag'] = nasaevent['mass']	
-		#print(milliseconds_since_epoch)
+		nasaevent['year'] = nasaevent['year'].split("-", 1)[0]
+	else:
+		nasaevents.remove(nasaevent)
 
-#for nasaevent in nasaevents:
-#	if "year" in nasaevent and "time" in nasaevent:
-#		print("nasaevent:" + str(nasaevent['time']))
-#		print("nasaevent:" + str(nasaevent['lat']))
-#		print("nasaevent:" + str(nasaevent['lng']))
-
-# order json
-#nasaevents.sort(key = lambda x:x["time"])
-
+# sort by year 
+nasaevents.sort(key=lambda x: x.get('year'))
 output_json = json.dumps(nasaevents)
-print(nasaevents)
-
+print(output_json)
